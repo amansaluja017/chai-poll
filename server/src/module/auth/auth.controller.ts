756e07ada3eb8bc3feaf6a24649d0f3572025397
aman <amansaluja017@gmail.com>
2026-05-15 21:36:05 +0530

    fix: update refreshToken cookie sameSite attribute to none and add maxAge to login response

--- server/src/module/auth/auth.controller.ts
+++ server/src/module/auth/auth.controller.ts
@@ -25,7 +25,7 @@ export const authenticate = async (req: Request, res: Response) => {
     res.cookie("refreshToken", refreshToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        sameSite: "none",
         maxAge: 1 * 24 * 60 * 60 * 1000,
     });
 
@@ -51,7 +51,8 @@ export const refresh = async (req: Request, res: Response) => {
     res.cookie("refreshToken", refreshToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        sameSite: "none",
+        maxAge: 1 * 24 * 60 * 60 * 1000,
     });
 
     ApiResponse.ok(res, "user login successfully", {
